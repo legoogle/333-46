841bb0e91c360bc0f27322419e3a675c29e09eaf
lekhotla <lekhotla.m@afrihost.com>
2018-09-21 14:55:05 +0200

    Adding new App (Vue)

---
+++ App-final/src/js/main.js
@@ -0,0 +1,24 @@
+// import npm modules
+import Vue from 'vue';
+
+import Home from './components/Home';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+         show: true,
+         isActive: false,
+        message: 'Hello Vue! Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue'
+    }
+});
+
+//new Vue({
+//  el: "#",
+//  data: {
+//
+//  }
+//})
+
+function myFunction() {
+    alert("Hello! I am an alert box!");
+}
---
+++ App-final/src/sass/components/App-final/src/js/main.js
@@ -0,0 +1,23 @@
+// import npm modules
+import Vue from 'vue';
+
+import Home from './components/Home';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+         show: true,
+        message: 'Hello Vue! Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue!Hello Vue'
+    }
+});
+
+//new Vue({
+//  el: "#",
+//  data: {
+//
+//  }
+//})
+
+function myFunction() {
+    alert("Hello! I am an alert box!");
+}
