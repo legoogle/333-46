6576985de448f475056d189c3ddc3ddd33849b6e
lekhotla <lekhotla.m@afrihost.com>
2018-10-26 07:39:27 +0200

    Playing around with vue components

--- App-final/src/js/main.js
+++ App-final/src/js/main.js
@@ -3,6 +3,13 @@ import Vue from 'vue';
 
 import Home from './components/Home';
 
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+
 const app = new Vue({
     el: '#app',
     data: {
